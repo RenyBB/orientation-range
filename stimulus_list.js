b4ae4be6d09412d25f1470ae1695b6207d6ebbd2
RenyBB <71499318+RenyBB@users.noreply.github.com>
2023-01-18 15:07:37 +0000

    Update stimulus_list.js

--- stimulus_list.js
+++ stimulus_list.js
@@ -16,7 +16,7 @@ while (current_phase <= end) {
 // practice stimuli for condition 1
 let training_stimuli_range1 = [
 	{ stimulus: "gabor_stimuli_matching/gabor_ori8_phase", orientation: 8},
-	{ stimulus: "gabor_stimuli_matching/gabor_ori170_phase", orientation: 179},
+	{ stimulus: "gabor_stimuli_matching/gabor_ori179_phase", orientation: 179},
 ];
 
 
